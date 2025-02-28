f6b68a56328264658e9c005da0324bc652bbda25
MarvinJWendt <git@marvinjwendt.com>
2025-02-28 10:27:03 +0100

    cleanup validation function

--- internal/pkg/service/validation.go
+++ internal/pkg/service/validation.go
@@ -21,33 +21,33 @@ func (s Service) Validate(w http.ResponseWriter, data any) bool {
 	}
 
 	err = s.validator.Struct(data)
-	if err != nil {
-		var invalidValidationError *validator.InvalidValidationError
-		if errors.As(err, &invalidValidationError) {
-			sendJSON(w, http.StatusBadRequest, api.ValidationError{
-				Message:    "validation failed: illegal argument was passed",
-				StatusCode: http.StatusBadRequest,
-			})
-		}
-
-		var result api.ValidationMessages
-		var errorsMap validator.ValidationErrors
-		errors.As(err, &errorsMap)
-		translatedErrors := errorsMap.Translate(s.translator)
-		for k, v := range translatedErrors {
-			keySplit := strings.Split(k, ".")
-			result = append(result, api.ValidationMessage{
-				Field:   strings.ToLower(keySplit[len(keySplit)-1]),
-				Message: v,
-			})
-		}
-		sendJSON(w, http.StatusBadRequest, api.ValidationError{
-			Message:    "validation failed",
-			StatusCode: http.StatusBadRequest,
-			Errors:     &result,
-		})
-		return false
+	if err == nil {
+		return true // no validation errors occurred
 	}
 
-	return true
+	var invalidValidationError *validator.InvalidValidationError
+	if errors.As(err, &invalidValidationError) {
+		sendJSON(w, http.StatusBadRequest, api.ValidationError{
+			Message:    "validation failed: illegal argument was passed",
+			StatusCode: http.StatusBadRequest,
+		})
+	}
+
+	var result api.ValidationMessages
+	var errorsMap validator.ValidationErrors
+	errors.As(err, &errorsMap)
+	translatedErrors := errorsMap.Translate(s.translator)
+	for k, v := range translatedErrors {
+		keySplit := strings.Split(k, ".")
+		result = append(result, api.ValidationMessage{
+			Field:   strings.ToLower(keySplit[len(keySplit)-1]),
+			Message: v,
+		})
+	}
+	sendJSON(w, http.StatusBadRequest, api.ValidationError{
+		Message:    "validation failed",
+		StatusCode: http.StatusBadRequest,
+		Errors:     &result,
+	})
+	return false
 }
